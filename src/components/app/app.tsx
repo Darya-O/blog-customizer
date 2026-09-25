fb092259525484604e2437db9c85045b7898c73b
Darya Ostreshko <dasha.ostreshko@yandex.ru>
2026-09-25 14:27:59 +0300

    PR10fix

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,30 +1,44 @@
-import { defaultArticleState } from '@/constants/articleProps.ts';
-import { clsx } from 'clsx';
-
-import { ArticleParamsForm } from '@components/article-params-form';
+import { useState } from 'react';
+import { CSSProperties } from 'react';
 
 import { Article } from '../article/Article';
-
-import type { CSSProperties } from 'react';
+import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './../../constants/articleProps';
 
 import styles from './app.module.scss';
 
-export const App = (): React.JSX.Element => {
-  return (
-    <main
-      className={clsx(styles.main)}
-      style={
-        {
-          '--font-family': defaultArticleState.fontFamilyOption.value,
-          '--font-size': defaultArticleState.fontSizeOption.value,
-          '--font-color': defaultArticleState.fontColor.value,
-          '--container-width': defaultArticleState.contentWidth.value,
-          '--bg-color': defaultArticleState.backgroundColor.value,
-        } as CSSProperties
-      }
-    >
-      <ArticleParamsForm />
-      <Article />
-    </main>
-  );
+export const App = () => {
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleApply = (newState: ArticleStateType) => {
+		setArticleState(newState);
+	};
+
+	const handleReset = () => {
+		setArticleState(defaultArticleState);
+	};
+
+	const cssVariables: CSSProperties = {
+		'--font-family': articleState.fontFamilyOption.value,
+		'--font-color': articleState.fontColor.value,
+		'--bg-color': articleState.backgroundColor.value,
+		'--container-width': articleState.contentWidth.value,
+		'--font-size': articleState.fontSizeOption.value,
+	} as React.CSSProperties;
+
+	return (
+		<main className={styles.main} style={cssVariables}>
+			<ArticleParamsForm
+				initialState={defaultArticleState}
+				currentState={articleState}
+				onApply={handleApply}
+				onReset={handleReset}
+			/>
+			<Article />
+		</main>
+	);
 };
